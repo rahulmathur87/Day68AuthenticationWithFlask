9ae553b306dd595dfcd14dd0687d38da3e3ad677
Rahul <rahulmathur87@gmail.com>
2024-08-29 02:10:42 +0530

    hashed passwords added

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def register():
         new_user = User(
             name=request.form.get("name"),
             email=request.form.get("email"),
-            password=request.form.get("password")
+            password=generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
         )
         db.session.add(new_user)
         db.session.commit()
